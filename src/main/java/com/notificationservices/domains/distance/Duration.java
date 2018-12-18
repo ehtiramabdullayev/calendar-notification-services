0278d1dae38c7d34da133584e02ad8be767986ef
Ehtiram Abdullayev <ehtiramabdullayev@gmail.com>
2018-12-18 14:17:10 +0100

    toString method removed

---
+++ src/main/java/com/notificationservices/domains/distance/Duration.java
@@ -0,0 +1,28 @@
+package com.notificationservices.domains.distance;
+
+/**
+ * @author Ehtiram_Abdullayev on 18.12.2018
+ * @project notificationservices
+ */
+public class Duration {
+    private String text;
+
+    private String value;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
